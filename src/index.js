146868ad371d122fab1d810582b341830f9bf4a1
Ekaterina <komsa_9191@mail.ru>
2023-08-01 14:46:21 +1000

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = String(n);
+    let result = '';
+    let i = 0;
+
+    while(i < str.length) {
+        if (n < 0) {
+            n = n * (-1);
+            str = String(n);
+        }
+        result = str[i] + result;
+        i += 1;
+    }
+    return Number(result);
+};
